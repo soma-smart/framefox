09de2325b8f953bd6b5f522fd8b6c6d9537934d2
RaphaelLEUROND <rleurond@soma-smart.com>
2024-12-12 16:16:35 +0100

    feat: dynamically set table name based on class name in AbstractEntity

--- src/entity/item_test.py
+++ src/entity/item_test.py
@@ -2,12 +2,10 @@ from src.core.orm.config.database import db
 from src.core.orm.abstract_entity import AbstractEntity
 
 
-# class ItemTest(db.Model):
 class ItemTest(AbstractEntity):
     """
     Example
     """
-    __tablename__ = 'items_test'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
