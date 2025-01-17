118b0c65b3f5bf9d333e62be99ef6f712bffa0d8
rleurond <leurond.raphael@gmail.com>
2025-01-17 10:03:59 +0100

    feat: upgrade create commands UI

--- framefox/terminal/commands/create/create_controller_command.py
+++ framefox/terminal/commands/create/create_controller_command.py
@@ -1,6 +1,7 @@
 from framefox.terminal.commands.abstract_command import AbstractCommand
 from framefox.terminal.common.class_name_manager import ClassNameManager
 from framefox.terminal.common.file_creator import FileCreator
+from framefox.terminal.common.input_manager import InputManager
 
 
 class CreateControllerCommand(AbstractCommand):
@@ -11,15 +12,25 @@ class CreateControllerCommand(AbstractCommand):
         self.controller_template = r'controller_template.jinja2'
         self.view_template = r'view_template.jinja2'
 
-    def execute(self, name: str):
+    def execute(self, name: str = None):
         """
         Create a simple controller with a exemple route and view.
 
         Args:
             name (str): The name of the controller.
         """
+        if name is None:
+            name = InputManager().wait_input("Controller name")
+            if name == '':
+                return
+
         if not ClassNameManager.is_snake_case(name):
-            print("\033[91mInvalid name. Must be in snake_case.\033[0m")
+            self.printer.print_msg(
+                "Invalid name. Must be in snake_case.",
+                theme="error",
+                linebefore=True,
+                newline=True
+            )
             return
 
         class_name = f"{ClassNameManager.snake_to_pascal(name)}Controller"
@@ -36,17 +47,27 @@ class CreateControllerCommand(AbstractCommand):
             "controller_class_name": class_name,
         }
 
-        FileCreator().create_file(
+        controller_path = FileCreator().create_file(
             template=self.controller_template,
             path=self.controller_path,
             name=f"{name}_controller",
             data=data_controller
         )
-        FileCreator().create_file(
+        view_path = FileCreator().create_file(
             template=self.view_template,
             path=self.view_path,
             name=view_name,
             data=data_view,
             format="html"
         )
-        print("\033[92mController and view created successfully.\033[0m")
+
+        self.printer.print_msg(
+            f"Controller successfully: {controller_path}",
+            theme="success",
+            linebefore=True,
+        )
+        self.printer.print_msg(
+            f"View successfully: {view_path}",
+            theme="success",
+            newline=True,
+        )
