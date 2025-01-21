ccce30642de65305c537a9eb5ce31afde3b5959a
rleurond <leurond.raphael@gmail.com>
2025-01-21 10:34:36 +0100

    chore: clean repository structure and improve code organization

--- src/controllers/admin_controller.py
+++
@@ -1,8 +0,0 @@
-from framefox.core.routing.decorator.route import Route
-from framefox.core.controller.abstract_controller import AbstractController
-
-
-class AdminController(AbstractController):
-    @Route("/admin", "index", methods=["GET"])
-    async def index(self):
-        return self.render("admin.html", {"controller_name":"AdminController"})
--- src/entity/tata.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import Field, Column, JSON
-from framefox.core.orm.abstract_entity import AbstractEntity
-from typing import List, Optional
-from datetime import datetime
-
-
-class Tata(AbstractEntity, table=True):
-    id: int = Field(default=None, primary_key=True)
-    name: str = Field(max_length=256)
