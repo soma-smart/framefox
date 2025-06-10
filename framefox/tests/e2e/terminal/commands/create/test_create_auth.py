4ac7ff9577f64d3afed4f3be756baa44575928a5
Vianney Bacoup <vbacoup@soma-smart.com>
2025-06-10 17:33:33 +0200

    Fix typo

--- framefox/tests/e2e/terminal/commands/create/test_create_auth.py
+++ framefox/tests/e2e/terminal/commands/create/test_create_auth.py
@@ -38,7 +38,7 @@ def test_create_auth_command_without_user_before_should_fail(init_project):
 
 
 @pytest.mark.parametrize("user_input", ["\n\n\n", "1\n\n\n", "1\n\nuser\n", "1\nauth\n\n"])
-def test_create_auth_command_with_user_before_should_succeed(user_input, create_user):
+def test_create_auth_command_with_user_before_should_succeed(user_input, create_user, init_project):
     result = exec_command(
         ["framefox", "create", "auth"],
         input_value=user_input
@@ -47,8 +47,8 @@ def test_create_auth_command_with_user_before_should_succeed(user_input, create_
     assert result.returncode == 0, f"Command should succeed with user before input"
 
 
-def test_create_auth_command_should_create_authenticator_file(create_user):
-    files = ["src/security/default_authenticator.py", "src/controllers/login_controller.py", "templates/security/login.html", "config/security.yaml"]
+def test_create_auth_command_should_create_authenticator_file(create_user, init_project):
+    files = ["src/security/default_authenticator.py", "src/controller/login_controller.py", "templates/security/login.html", "config/security.yaml"]
 
     exec_command(
         ["framefox", "create", "auth"],
@@ -72,7 +72,7 @@ def test_create_auth_command_should_create_authenticator_file(create_user):
     assert main.get("provider") == "app_user_provider", "Incorrect provider in firewalls.main in security.yaml"
 
 
-def test_create_auth_command_when_already_done_should_fail(create_user):
+def test_create_auth_command_when_already_done_should_fail(create_user, init_project):
     exec_command(
         ["framefox", "create", "auth"],
         input_value="\n\n\n"
